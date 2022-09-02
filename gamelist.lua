3d38cb3ded5f60c162d48a1025323e54a3b0b8e8
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-09-03 00:08:15 +0700

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,4 +1,4 @@
 return {
-[402122991] = "",
-[155615604] = ""
+[402122991] = "https://raw.githubusercontent.com/bobtheblob/sads/main/games/redwoodprison.lua",
+[155615604] = "https://raw.githubusercontent.com/bobtheblob/sads/main/games/prisonlife.lua"
 }

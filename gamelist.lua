a7b1d4767717555787cca5d126392dffeb8e481a
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-09-22 16:34:21 +0700

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,4 +1,5 @@
 return {
-[402122991] = "https://raw.githubusercontent.com/bobtheblob/sads/main/games/redwoodprison.lua",
-[155615604] = "https://raw.githubusercontent.com/bobtheblob/sads/main/games/prisonlife.lua"
+[402122991] = "https://raw.githubusercontent.com/bobtheblob/roblox-scripts/main/games/redwoodprison.lua",
+[155615604] = "https://raw.githubusercontent.com/bobtheblob/roblox-scripts/main/games/prisonlife.lua",
+[286090429] = "https://raw.githubusercontent.com/bobtheblob/roblox-scripts/main/games/arsenal.lua",
 }

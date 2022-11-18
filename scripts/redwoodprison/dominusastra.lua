61f8a410d98599b67378c9fda7de29572c23f375
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-11-18 21:23:12 +0700

    Update dominusastra.lua

--- scripts/redwoodprison/dominusastra.lua
+++ scripts/redwoodprison/dominusastra.lua
@@ -542,7 +542,7 @@ function dmg(v,d)
 	task.spawn(function()
 		local humano = v:FindFirstChildOfClass("Humanoid")
 		if not humano then return end
-		char:PivotTo(humano.RootPart.CFrame*CFrame.new(humano.MoveDirection.X*4,0,2+humano.MoveDirection.Z*4))
+		char:PivotTo(humano.RootPart.CFrame*CFrame.new(humano.MoveDirection.X*4,0,humano.MoveDirection.Z*4))
 		local opos = v:GetPivot()
 		wait(.25)
 		getgenv()._upsilonLibrary.FireServer("dealMeleeDamage", humano, d)
